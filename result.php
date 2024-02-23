0f309fd5738cfa8a691881d6d550f17e01c3f978
sisdream <sissipascale2@gmail.com>
2024-02-23 16:00:24 +0100

    feat:second file

---
+++ result.php
@@ -0,0 +1,6 @@
+<!-- php -->
+<?php
+    // php variable
+    $lorem = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum quis, impedit, ipsa officiis atque placeat quod veritatis, at tenetur quibusdam quas ipsam fuga odit harum saepe eius nisi iusto veniam.';
+        
+?>
