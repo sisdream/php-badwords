7d31466a6b776931b4be67168bf1efea1b6418d0
sisdream <sissipascale2@gmail.com>
2024-02-23 15:57:29 +0100

    feat: form

--- index.php
+++ index.php
@@ -1,11 +1,24 @@
+   <!-- php -->
+   <?php
+            // php variable
+        $lorem = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum quis, impedit, ipsa officiis atque placeat quod veritatis, at tenetur quibusdam quas ipsam fuga odit harum saepe eius nisi iusto veniam.';
+        
+    ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php-badwords</title>
 </head>
 <body>
-    
+    <div class="container mt-5">
+        <form action="index.php" method="get">
+            <input type="text" name="search" placeholder="censura parola">
+            <button> invia </button>
+        </form>
+    </div>
 </body>
 </html>
